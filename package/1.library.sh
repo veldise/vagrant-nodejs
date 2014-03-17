9920f8bec294cb58d2652c1825bdb12cd58745f1
최영철 <veldise@gmail.com>
2014-03-17 07:57:46 +0100

    .gitconfig 파일 복사가 안되는 문제 수정

--- package/1.library.sh
+++ package/1.library.sh
@@ -3,7 +3,7 @@ echo "- Utilities or Library Install Start"
 yum -y install wget make gcc gcc-c++ bzip2-devel openssl-devel git
 
 echo "- set Git global config"
-confPath=/home/vagrant/.gitconfig
+confPath='/home/vagrant/.gitconfig'
 cp /vagrant/package/gitconfig $confPath
 chown vagrant:vagrant $confPath
 chmod 644 $confPath
