bb7c091b0d1d2cee947ce484406e927226d17f40
황인용 <hiy2259@me.com>
2014-10-16 13:56:30 +0900

    mongodbCreateUser 코드 수정

--- package/mongodbCreateUser.js
+++ package/mongodbCreateUser.js
@@ -1,16 +1,22 @@
+// select db
 use admin
+// create admin
 db.createUser({
-    "user": "admin",
-    "pwd": "admin.",
-    "roles": [ "root" ]
+    user: "admin",
+    pwd: "admin.",
+    roles: [{
+        role: "userAdminAnyDatabase",
+        db: "admin"
+    }]
 });
-db.auth("admin", "admin.");
-
+// select db
 use mobigen
+// create monbigen
 db.createUser({
-    "user": "mobigen",
-    "pwd": "mobigenUI.",
-    "roles": [
-        { "role": "dbOwner", "db": "mobigen" }
-    ]
+    user: "mobigen",
+    pwd: "mobigenUI.",
+    roles: [{
+        role: "dbOwner",
+        db: "mobigen"
+    }]
 });
