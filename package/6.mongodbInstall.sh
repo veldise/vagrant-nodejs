b7a4f3d519ccf55ccafc2714c62e253c1a8d8f0d
최영철 <veldise@gmail.com>
2014-10-16 05:10:57 +0000

    mongodb install script에 주석 추가

--- package/6.mongodbInstall.sh
+++ package/6.mongodbInstall.sh
@@ -11,25 +11,27 @@ if [ "${mongo_ver}" == "MongoDB shell version: "$INSTALL_VER ]; then
 else
 	echo "Yum Install MongoDB"
 
+	# copy files
 	cp /vagrant/package/mongodb.repo /etc/yum.repos.d/mongodb.repo
-
 	cp /vagrant/package/mongodbCreateUser.js mongodbCreateUser.js
 
 	yum install -y mongodb-org-$INSTALL_VER &> mongodb_install.log
 
+	# run DB
 	service mongod start
 
 	# create user
 	mongo < mongodbCreateUser.js
 
+	# stop DB & modify config
 	service mongod stop
+	sed 's/#auth=true/auth=true/' /etc/mongod.conf >> /etc/mongod.conf.tmp
+	mv /etc/mongod.conf.tmp /etc/mongod.conf
 
-	# modify config
-	sed 's/#auth=true/auth=true/' /etc/mongod.conf >> /etc/mongod2.conf
-	mv /etc/mongod2.conf /etc/mongod.conf
-
+	# restart DB
 	/etc/init.d/mongod start
 
+	# register chkconfig (Launch DB when VM booting)
 	chkconfig --levels 235 mongod on
 
 	# remove files
